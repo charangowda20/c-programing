fd568e6fd49efdecbd70f9f176443c65a6c58463
charangowda20 <charangowdak255@gmail.com>
2026-08-04 20:12:26 +0530

    Implement mathematical functions in MathFunction.c
    Fix missing closing parenthesis in floor function call and add newline at end of file.

---
+++ MathFunction.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+void main(){
+  printf("%f \n",sqrt(25));
+printf("%f \n",round(8.7));
+printf("%f \n",pow(2,3));
+printf("%f \n",floor(5.7);
+printf("%f \n",ceil(5.7));
+printf("%f \n",log(0));
+}
