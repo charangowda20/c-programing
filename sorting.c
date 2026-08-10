f826f484661cb1b67d4bfccbdcc272f1869c95fd
charangowda20 <charangowdak255@gmail.com>
2026-08-10 22:08:00 +0530

    Add bubble sort implementation in sorting.c
    Implement a bubble sort algorithm to sort an array of integers in ascending order and display the sorted array.

---
+++ sorting.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+    int arr[100], n;
+    int temp;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n);
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+    printf("Array in ascending order:\n");
+
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
